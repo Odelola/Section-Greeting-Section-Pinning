7068d4a678c0beba9c4fa66d7dcb2132593b0d72
Odelola <integralandintegrations@gmail.com>
2022-07-14 21:38:19 +0100

    Changes to UI

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
-gsap.registerPlugin(ScrollTrigger);
+const body = document.body;
+const html = document.documentElement;
+const scrollProgressBar = document.getElementsByClassName("scroll-progress_bar")[0];
+const firstSection = document.getElementsByClassName("two")[0];
+const height = Math.max(body.getBoundingClientRect().height, html.getBoundingClientRect().height);
+console.log(scrollProgressBar)
+console.log(height)
+console.log(firstSection.offsetWidth)
+
+function updateScrollProgress() {
+  // console.log(scrollProgressBar.offsetWidth)
+  console.log(window.scrollY)
+}
+
+updateScrollProgress();
+
+window.addEventListener("scroll", updateScrollProgress)
+
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
 
 gsap.utils.toArray(".container section").forEach((section) => {
   ScrollTrigger.create({
